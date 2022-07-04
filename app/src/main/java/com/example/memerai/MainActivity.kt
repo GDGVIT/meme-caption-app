f1ff666431bde6587505165fdf4014840b220874
PrashannaR <prashanna.rajbhandari1@gmail.com>
2022-07-04 13:26:55 +0545

    feat: uploads images from the gallery

--- app/src/main/java/com/example/memerai/MainActivity.kt
+++ app/src/main/java/com/example/memerai/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.memerai
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        uploadBtn.isEnabled = false
+
+        //upload image
+        uploadBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/"
+            startActivityForResult(intent, 1)
+        }
+
+        //gallery permissions
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 2)
+        }else{
+            uploadBtn.isEnabled = true
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK){
+            photoIV.setImageURI(data?.data)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            uploadBtn.isEnabled = true
+        }
     }
 }
