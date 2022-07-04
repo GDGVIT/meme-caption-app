54d9a1b3e9d4a7847345c30e9647331dfef8c366
PrashannaR <prashanna.rajbhandari1@gmail.com>
2022-07-04 13:46:49 +0545

    feat: captures image using the camera

--- app/src/main/java/com/example/memerai/MainActivity.kt
+++ app/src/main/java/com/example/memerai/MainActivity.kt
@@ -4,10 +4,19 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResult
 import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
+private lateinit var photoFile: File
+private const val FILE_NAME = "photo.jpg"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +24,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         uploadBtn.isEnabled = false
+        captureBtn.isEnabled = false
+
+        //permissions
+        //gallery permissions
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 2)
+        }else{
+            uploadBtn.isEnabled = true
+        }
+
+        //camera permissions
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 3)
+        }else{
+            captureBtn.isEnabled = true
+        }
 
         //upload image
         uploadBtn.setOnClickListener {
@@ -23,17 +48,30 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(intent, 1)
         }
 
-        //gallery permissions
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 2)
-        }else{
-            uploadBtn.isEnabled = true
+        //capture image
+        captureBtn.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            photoFile = getPhotoFile(FILE_NAME)
+
+            val fileProvider = FileProvider.getUriForFile(this, "com.example.fileprovider", photoFile)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+            startActivityForResult(intent, 4)
         }
+
+
+    }
+
+    private fun getPhotoFile(fileName: String): File {
+        val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName, ".jpg", storageDirectory)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if(requestCode == 1 && resultCode == Activity.RESULT_OK){
             photoIV.setImageURI(data?.data)
+        }else if(requestCode == 4 && resultCode == Activity.RESULT_OK){
+            val img = BitmapFactory.decodeFile(photoFile.absolutePath)
+            photoIV.setImageBitmap(img)
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
@@ -46,6 +84,8 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if(requestCode == 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
             uploadBtn.isEnabled = true
+        }else if(requestCode == 3 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            captureBtn.isEnabled = true
         }
     }
 }
